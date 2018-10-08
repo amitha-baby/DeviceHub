a472a643335fa82e7f28757d8b7c3e8b71e480de
amitha.baby <amitha.baby@cabotsolutions.com>
2018-10-08 16:16:10 +0530

    Device Available is fetched from local storage

--- js/service.js
+++ js/service.js
@@ -1,49 +1,86 @@
-const tbody = document.getElementById("device-list");
-let device_storage = localStorage.getItem('device-list') ? JSON.parse(localStorage.getItem('device-list')) : [];
+'use strict';
+let deviceDataArray = localStorage.getItem('deviceList') ? JSON.parse(localStorage.getItem('deviceList')) : [];
+var jsonObject;
 
-localStorage.setItem('device-list', JSON.stringify(device_storage));
-const data = JSON.parse(localStorage.getItem('device-list'));
+localStorage.setItem('deviceList', JSON.stringify(deviceDataArray));
+const dataFromLocalStorage = JSON.parse(localStorage.getItem('deviceList'));
  
 
 var request = new XMLHttpRequest();
-var theurl='https://script.googleusercontent.com/a/macros/cabotsolutions.com/echo?user_content_key=zCJnmt801-Kq6J7pODUnA_Klq7NIkTrmt8YyROy-QXtMxHmdIymRSAv4CZuF7UMMyUU-raC-yb_ZI-9Nrhp0zK5YJMUSBjSNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP8N3YZ91GNx49FXyl5euMLmjnFAkueCBXiaVRYyZM5cQbphKptrKjl7-hDBmQBEtxgKwTgRjX0_05v2N8Ym_oTBPLjKh6_FEGNGobvHn3QJCRhmlNqT-jbZ&lib=MvYB3dXCLYXIaqwPl1O-F3ZRuoRp3PEkk';
-    request.open('GET',theurl, true);
+var url = 'https://script.googleusercontent.com/a/macros/cabotsolutions.com/echo?user_content_key=zCJnmt801-Kq6J7pODUnA_Klq7NIkTrmt8YyROy-QXtMxHmdIymRSAv4CZuF7UMMyUU-raC-yb_ZI-9Nrhp0zK5YJMUSBjSNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP8N3YZ91GNx49FXyl5euMLmjnFAkueCBXiaVRYyZM5cQbphKptrKjl7-hDBmQBEtxgKwTgRjX0_05v2N8Ym_oTBPLjKh6_FEGNGobvHn3QJCRhmlNqT-jbZ&lib=MvYB3dXCLYXIaqwPl1O-F3ZRuoRp3PEkk';
+    request.open('GET',url, true);
         
     request.onload = function () {
-    var json_object = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-        var json_object = eval("(" + this.response + ")");
-        var device_array = json_object["user"];
-   
-        function additem(){
-            for (var device = 0; device < device_array.length; device++) {
+        var json = JSON.parse(this.response);
+        if (request.status >= 200 && request.status < 400) {
+            var json = eval("(" + this.response + ")");
+            jsonObject = json["user"];
+            const tBody = document.getElementById("deviceList");
 
-                const tr = document.createElement('tr');
+            function additem() {
+                for (var device = 0; device < jsonObject.length; device++) {
 
-                tr.innerHTML = `
-                <td>${device_array[device].SL_NO}</td>
-                <td>${device_array[device].Name}</td>
-                <td>${device_array[device].Status}</td>
-                <td>${device_array[device].Model}</td>
-                <td>${device_array[device].ID}</td>
-                <td>${device_array[device].Owner}</td>
-                <td>${device_array[device].Location}</td>
-                `;
+                    const tr = document.createElement('tr');
 
-                tbody.appendChild(tr);
+                    tr.innerHTML = `
+                    <td>${jsonObject[device].SLNO}</td>
+                    <td>${jsonObject[device].NAME}</td>
+                    <td>${jsonObject[device].STATUS}</td>
+                    <td>${jsonObject[device].MODEL}</td>
+                    <td>${jsonObject[device].ID}</td>
+                    <td>${jsonObject[device].OWNER}</td>
+                    <td>${jsonObject[device].LOCATION}</td>
+                    `;
+
+                    tBody.appendChild(tr);
+                }
             }
-        }
      
-        device_storage.push(tbody.value);
-        localStorage.setItem('device-list', JSON.stringify(device_array));
+        deviceDataArray.push(tBody.value);
+        localStorage.setItem('deviceList', JSON.stringify(jsonObject));
             additem();
 
     } else {
             const errorMessage = document.createElement('marquee');
             errorMessage.textContent = `Gah, it's not working!`;
-            tbody.appendChild(errorMessage);
+            tBody.appendChild(errorMessage);
           }
         }
    
     request.send();
-    
+
+
+
+    var availableButton = document.getElementById('available_btn');
+    var noCharger = document.getElementById('nocharger_btn');
+    var missing = document.getElementById('missing_btn');
+    var notWorking = document.getElementById('notworking_btn');
+
+    availableButton.addEventListener('click', filterItems);
+    noCharger.addEventListener('click', filterItems);
+    missing.addEventListener('click', filterItems);
+    notWorking.addEventListener('click', filterItems);
+
+  
+    function filterItems(e){
+        e.preventDefault;
+        $("#deviceList").empty();
+        const tBody = document.getElementById("deviceList");
+        for (var device = 0; device < jsonObject.length; device++) {
+
+            if (deviceDataArray[device].STATUS == "Available") {
+                const tr = document.createElement('tr');
+                        tr.innerHTML = `
+                        <td>${deviceDataArray[device].SLNO}</td>
+                        <td>${deviceDataArray[device].NAME}</td>
+                        <td>${deviceDataArray[device].STATUS}</td>
+                        <td>${deviceDataArray[device].MODEL}</td>
+                        <td>${deviceDataArray[device].ID}</td>
+                        <td>${deviceDataArray[device].OWNER}</td>
+                        <td>${deviceDataArray[device].LOCATION}</td>
+                        `;
+                      
+                        tBody.appendChild(tr);
+            } 
+        }           
+    }
