83f6deec759803cd7659ec7253d25fe867957a48
amitha.baby <amitha.baby@cabotsolutions.com>
2018-10-11 09:46:30 +0530

    Device overview is gets data dynamically

--- js/main.js
+++ js/main.js
@@ -1,4 +1,174 @@
-$.getScript( "js/service.js" , function(){
-    alert(tbody);
-});
+'use strict';
+let deviceDataArray = localStorage.getItem('deviceList') ? JSON.parse(localStorage.getItem('deviceList')) : [];
+var jsonObject;
 
+localStorage.setItem('deviceList', JSON.stringify(deviceDataArray));
+const dataFromLocalStorage = JSON.parse(localStorage.getItem('deviceList'));
+ 
+
+var request = new XMLHttpRequest();
+var url = 'https://script.googleusercontent.com/a/macros/cabotsolutions.com/echo?user_content_key=IGUdrzEd5bHowjSL73tAMQouPMaK3VhGtNOsdqWbMugGYIYzNfzcPWNopRbl9yXKDu2885-wArBrcTXx9q-Yqdp1Q8GFEIk4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP8N3YZ91GNx49FXyl5euMLmjnFAkueCBXj0iX0FmGGH8eysCHO9HnBESumQduuF82BG65TUsf-URlO8TeqEh96kujRJXaOC0_H5yDnhMzfu31zLqmv46PQV&lib=MMKXgi_qUFWSt0nTLnoLjrkylSQPUcriW';
+    request.open('GET',url, true);
+        
+    request.onload = function () {
+        var json = JSON.parse(this.response);
+        if (request.status >= 200 && request.status < 400) {
+            var json = eval("(" + this.response + ")");
+            jsonObject = json["user"];
+            const tBody = document.getElementById("deviceList");
+
+            function additem() {
+                for (var device = 0; device < jsonObject.length; device++) {
+
+                    const tr = document.createElement('tr');
+
+                    tr.innerHTML = `
+                    <td>${jsonObject[device].MANUFACTURER}</td>
+                    <td>${jsonObject[device].NAME}</td>
+                    <td>${jsonObject[device].STATUS}</td>
+                    <td>${jsonObject[device].MODEL}</td>
+                    <td>${jsonObject[device].SERIALNO}</td>
+                    <td>${jsonObject[device].IMEINO}</td>
+                    <td>${jsonObject[device].CHARGE}</td>
+                    `;
+
+                    tBody.appendChild(tr);
+                }
+            }
+     
+        deviceDataArray.push(tBody.value);
+        localStorage.setItem('deviceList', JSON.stringify(jsonObject));
+            additem();
+
+    } else {
+            const errorMessage = document.createElement('marquee');
+            errorMessage.textContent = `Gah, it's not working!`;
+            tBody.appendChild(errorMessage);
+          }
+        }
+   
+    request.send();
+
+
+    
+    class availableDevices {
+
+        filterItems(e) {
+            e.preventDefault;
+            $("#deviceList").empty();
+            const tBody = document.getElementById("deviceList");
+            for (var device = 0; device < jsonObject.length; device++) {
+
+                if (deviceDataArray[device].STATUS == "Available") {
+                    const tr = document.createElement('tr');
+                    tr.innerHTML = `
+                    <td>${jsonObject[device].MANUFACTURER}</td>
+                    <td>${jsonObject[device].NAME}</td>
+                    <td>${jsonObject[device].STATUS}</td>
+                    <td>${jsonObject[device].MODEL}</td>
+                    <td>${jsonObject[device].SERIALNO}</td>
+                    <td>${jsonObject[device].IMEINO}</td>
+                    <td>${jsonObject[device].CHARGE}</td>
+                    `;
+                        
+                            tBody.appendChild(tr);
+                } 
+            }
+        }         
+    }
+
+    class noChargerDevices {
+
+        filterItems(e) {
+            e.preventDefault;
+            $("#deviceList").empty();
+            const tBody = document.getElementById("deviceList");
+            for (var device = 0; device < jsonObject.length; device++) {
+
+                if (deviceDataArray[device].STATUS == "No Charger") {
+                    const tr = document.createElement('tr');
+                    tr.innerHTML = `
+                    <td>${jsonObject[device].MANUFACTURER}</td>
+                    <td>${jsonObject[device].NAME}</td>
+                    <td>${jsonObject[device].STATUS}</td>
+                    <td>${jsonObject[device].MODEL}</td>
+                    <td>${jsonObject[device].SERIALNO}</td>
+                    <td>${jsonObject[device].IMEINO}</td>
+                    <td>${jsonObject[device].CHARGE}</td>
+                    `;
+                        
+                            tBody.appendChild(tr);
+                } 
+            }
+        }         
+    }
+
+    class notWorkingDevices {
+
+        filterItems(e) {
+            e.preventDefault;
+            $("#deviceList").empty();
+            const tBody = document.getElementById("deviceList");
+            for (var device = 0; device < jsonObject.length; device++) {
+
+                if (deviceDataArray[device].STATUS == "Not Working") {
+                    const tr = document.createElement('tr');
+                    tr.innerHTML = `
+                    <td>${jsonObject[device].MANUFACTURER}</td>
+                    <td>${jsonObject[device].NAME}</td>
+                    <td>${jsonObject[device].STATUS}</td>
+                    <td>${jsonObject[device].MODEL}</td>
+                    <td>${jsonObject[device].SERIALNO}</td>
+                    <td>${jsonObject[device].IMEINO}</td>
+                    <td>${jsonObject[device].CHARGE}</td>
+                    `;
+                        
+                            tBody.appendChild(tr);
+                } 
+            }
+        }            
+    }
+
+    class missingDevices {
+        
+        filterItems(e) {
+            e.preventDefault;
+            $("#deviceList").empty();
+            const tBody = document.getElementById("deviceList");
+            for (var device = 0; device < jsonObject.length; device++) {
+
+                if (deviceDataArray[device].STATUS == "Missing") {
+                    const tr = document.createElement('tr');
+                    tr.innerHTML = `
+                    <td>${jsonObject[device].MANUFACTURER}</td>
+                    <td>${jsonObject[device].NAME}</td>
+                    <td>${jsonObject[device].STATUS}</td>
+                    <td>${jsonObject[device].MODEL}</td>
+                    <td>${jsonObject[device].SERIALNO}</td>
+                    <td>${jsonObject[device].IMEINO}</td>
+                    <td>${jsonObject[device].CHARGE}</td>
+                    `;
+                        
+                            tBody.appendChild(tr);
+                } 
+            }
+        }            
+    }
+
+
+    let d1 = new availableDevices();
+    let d2 = new noChargerDevices();
+    let d3= new missingDevices();
+    let d4 = new notWorkingDevices();
+
+    var availableButton = document.getElementById('available_btn');
+    var noCharger = document.getElementById('nocharger_btn');
+    var missing = document.getElementById('missing_btn');
+    var notWorking = document.getElementById('notworking_btn');
+
+    availableButton.addEventListener('click',d1.filterItems);
+    noCharger.addEventListener('click', d2.filterItems);
+    missing.addEventListener('click', d3.filterItems);
+    notWorking.addEventListener('click', d4.filterItems);
+
+  
--- js/service.js
+++ js/service.js
@@ -1,13 +1,14 @@
 'use strict';
 let deviceDataArray = localStorage.getItem('deviceList') ? JSON.parse(localStorage.getItem('deviceList')) : [];
-var jsonObject;
+var jsonObject="";
+
 
 localStorage.setItem('deviceList', JSON.stringify(deviceDataArray));
 const dataFromLocalStorage = JSON.parse(localStorage.getItem('deviceList'));
  
 
 var request = new XMLHttpRequest();
-var url = 'https://script.googleusercontent.com/a/macros/cabotsolutions.com/echo?user_content_key=zCJnmt801-Kq6J7pODUnA_Klq7NIkTrmt8YyROy-QXtMxHmdIymRSAv4CZuF7UMMyUU-raC-yb_ZI-9Nrhp0zK5YJMUSBjSNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP8N3YZ91GNx49FXyl5euMLmjnFAkueCBXiaVRYyZM5cQbphKptrKjl7-hDBmQBEtxgKwTgRjX0_05v2N8Ym_oTBPLjKh6_FEGNGobvHn3QJCRhmlNqT-jbZ&lib=MvYB3dXCLYXIaqwPl1O-F3ZRuoRp3PEkk';
+var url = 'https://script.googleusercontent.com/a/macros/cabotsolutions.com/echo?user_content_key=IGUdrzEd5bHowjSL73tAMQouPMaK3VhGtNOsdqWbMugGYIYzNfzcPWNopRbl9yXKDu2885-wArBrcTXx9q-Yqdp1Q8GFEIk4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP8N3YZ91GNx49FXyl5euMLmjnFAkueCBXj0iX0FmGGH8eysCHO9HnBESumQduuF82BG65TUsf-URlO8TeqEh96kujRJXaOC0_H5yDnhMzfu31zLqmv46PQV&lib=MMKXgi_qUFWSt0nTLnoLjrkylSQPUcriW';
     request.open('GET',url, true);
         
     request.onload = function () {
@@ -23,13 +24,13 @@ var url = 'https://script.googleusercontent.com/a/macros/cabotsolutions.com/echo
                     const tr = document.createElement('tr');
 
                     tr.innerHTML = `
-                    <td>${jsonObject[device].SLNO}</td>
+                    <td>${jsonObject[device].MANUFACTURER}</td>
                     <td>${jsonObject[device].NAME}</td>
                     <td>${jsonObject[device].STATUS}</td>
                     <td>${jsonObject[device].MODEL}</td>
-                    <td>${jsonObject[device].ID}</td>
-                    <td>${jsonObject[device].OWNER}</td>
-                    <td>${jsonObject[device].LOCATION}</td>
+                    <td>${jsonObject[device].SERIALNO}</td>
+                    <td>${jsonObject[device].IMEINO}</td>
+                    <td>${jsonObject[device].IMEINO}</td>
                     `;
 
                     tBody.appendChild(tr);
@@ -48,39 +49,55 @@ var url = 'https://script.googleusercontent.com/a/macros/cabotsolutions.com/echo
         }
    
     request.send();
+    
+    
 
+    let AvailableObj = {
 
+        deviceName : 'Available',
+        getItems : function filterItems(e) {
+                    e.preventDefault;
+                    $("#deviceList").empty();
+                    const tBody = document.getElementById("deviceList");
+                    for (var device = 0; device < jsonObject.length; device++) {
+                        
+                            if (deviceDataArray[device].STATUS == this.deviceName) {
+                                console.log(this.deviceName);
+                                    const tr = document.createElement('tr');
+                                    tr.innerHTML = `
+                                    <td>${jsonObject[device].MANUFACTURER}</td>
+                                    <td>${jsonObject[device].NAME}</td>
+                                    <td>${jsonObject[device].STATUS}</td>
+                                    <td>${jsonObject[device].MODEL}</td>
+                                    <td>${jsonObject[device].SERIALNO}</td>
+                                    <td>${jsonObject[device].IMEINO}</td>
+                                    <td>${jsonObject[device].IMEINO}</td>
+                                    `;
+                                
+                                    tBody.appendChild(tr);
+                        } 
+                    }
+                }         
+    };
+
+    let NochargerObj = { deviceName : 'No Charger'};
+
+    let MissingObj = { deviceName : 'Missing'};
+    
+    let NotworkingObj = {deviceName : 'Not Working'};
 
     var availableButton = document.getElementById('available_btn');
     var noCharger = document.getElementById('nocharger_btn');
     var missing = document.getElementById('missing_btn');
     var notWorking = document.getElementById('notworking_btn');
 
-    availableButton.addEventListener('click', filterItems);
-    noCharger.addEventListener('click', filterItems);
-    missing.addEventListener('click', filterItems);
-    notWorking.addEventListener('click', filterItems);
+    noCharger.addEventListener('click', AvailableObj.getItems);
+    noCharger.addEventListener('click', AvailableObj.getItems.call(NochargerObj));
+    missing.addEventListener('click', AvailableObj.getItems.call(MissingObj));
+    notWorking.addEventListener('click', AvailableObj.getItems.call(NotworkingObj));
+
+
+
+
 
   
-    function filterItems(e){
-        e.preventDefault;
-        $("#deviceList").empty();
-        const tBody = document.getElementById("deviceList");
-        for (var device = 0; device < jsonObject.length; device++) {
-
-            if (deviceDataArray[device].STATUS == "Available") {
-                const tr = document.createElement('tr');
-                        tr.innerHTML = `
-                        <td>${deviceDataArray[device].SLNO}</td>
-                        <td>${deviceDataArray[device].NAME}</td>
-                        <td>${deviceDataArray[device].STATUS}</td>
-                        <td>${deviceDataArray[device].MODEL}</td>
-                        <td>${deviceDataArray[device].ID}</td>
-                        <td>${deviceDataArray[device].OWNER}</td>
-                        <td>${deviceDataArray[device].LOCATION}</td>
-                        `;
-                      
-                        tBody.appendChild(tr);
-            } 
-        }           
-    }
